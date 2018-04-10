3085f614374dcf93ef458a2340ea5b2217384a98
Friedrich Lindenberg <friedrich@pudo.org>
2018-04-10 18:01:11 +0200

    Undo the file name change in 0.4.0.

--- storagelayer/file.py
+++ storagelayer/file.py
@@ -1,12 +1,12 @@
 import os
 import shutil
 import logging
+from normality import safe_filename
 
 from storagelayer.archive import Archive
 from storagelayer.util import checksum, decode_path, BUF_SIZE
 
 log = logging.getLogger(__name__)
-FILENAME = 'data'
 
 
 class FileArchive(Archive):
@@ -22,12 +22,6 @@ class FileArchive(Archive):
         if prefix is None:
             return
         path = os.path.join(self.path, prefix)
-        default_path = os.path.join(path, FILENAME)
-        if os.path.exists(default_path):
-            return default_path
-
-        # this is here for compatibility with older versions of
-        # storagelayer which generated file names:
         try:
             for file_name in os.listdir(path):
                 return os.path.join(path, file_name)
@@ -49,7 +43,8 @@ class FileArchive(Archive):
         except Exception:
             return content_hash
 
-        archive_path = os.path.join(archive_path, FILENAME)
+        file_name = safe_filename(file_path, default='data')
+        archive_path = os.path.join(archive_path, file_name)
         with open(file_path, 'rb') as fin:
             with open(archive_path, 'wb') as fout:
                 shutil.copyfileobj(fin, fout, BUF_SIZE)
